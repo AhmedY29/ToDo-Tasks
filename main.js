f2fb108b00e4bb305a371f36fafeffd1c83814ea
AhmedY29 <ahmedys2001@gmail.com>
2025-05-12 20:33:19 +0300

    complete todo

---
+++ main.js
@@ -0,0 +1,169 @@
+// Buttons
+
+let addBtn = document.getElementById("add-todo");
+let deleteBtn = document.getElementById("delete-btn");
+let checkBtn = document.getElementById("check-todo");
+let cancelBtn = document.getElementById("cancel-btn");
+let closeDialog = document.getElementById("close-dialog");
+let addDialogBtn = document.getElementById("add-btn");
+let editBtn = document.getElementById("edit-btn");
+
+// Elements
+let todosContent = document.getElementById("todos-content");
+let todosItem = document.getElementById("todo-item");
+let addDialog = document.getElementById("add-todo-dialog");
+let deleteDialog = document.getElementById("delete-todo-dialog");
+let deleteMsg = document.getElementById("delete-msg");
+let tasksCount = document.getElementById("tasks-count");
+
+let editDialog = document.getElementById("edit-todo-dialog");
+let closeEditDialog = document.getElementById("close-edit-dialog");
+// Inputs
+let inputAdd = document.getElementById("todo-title");
+let inputEdit = document.getElementById("edit-todo-title");
+
+let todos = [
+  {
+    id: 1,
+    title: "Do Home Work 1",
+    check: false,
+  },
+  {
+    id: 2,
+    title: "Do Home Work 1",
+    check: false,
+  },
+  {
+    id: 3,
+    title: "Do Home Work 1",
+    check: false,
+  },
+  {
+    id: 4,
+    title: "Do Home Work 1",
+    check: false,
+  },
+];
+
+// Local Storage
+function storeTodos() {
+  let todosString = JSON.stringify(todos);
+  localStorage.setItem("todos", todosString);
+}
+
+function getStoreTodos() {
+  let todosObj = JSON.parse(localStorage.getItem("todos"));
+  todos = todosObj ?? [];
+}
+
+getStoreTodos();
+
+function getTodos() {
+  todosContent.innerHTML = "";
+  tasksCount.innerText = `Total Tasks: ${todos.length} | Uncompleted tasks: ${
+    todos.filter((e) => e.check == false).length
+  }`;
+  todos.forEach((item, index) => {
+    todosContent.innerHTML += `
+              <div id="todo-item" class="todo-item ${
+                item.check ? "todo-item-done" : ""
+              }">
+              <div class="btns">
+                  <button id="check-todo" onclick="handleCheckTodo(${index})" class="btn btn-check ${
+      item.check ? "btn-check-true" : ""
+    }">
+                    <span class="material-symbols-outlined"> check </span>
+                  </button>
+                  <button id="check-todo" onclick="handelEditTodo(${index})" class="btn btn-edit">
+                   <span class="material-symbols-outlined">
+edit
+</span>
+                  </button>
+                  </div>
+
+                  <h4>${item.title}</h4>
+                  <button id="delete-todo" onclick="handleOpenDelete(${index})" class="btn btn-delete">
+                    <span class="material-symbols-outlined">
+                      delete
+                    </span>
+                  </button>
+                </div>
+          `;
+  });
+}
+
+getTodos();
+
+addBtn.addEventListener("click", () => {
+  console.log("i'm listen");
+  addDialog.open = true;
+});
+
+closeDialog.addEventListener("click", () => {
+  console.log("i'm listen");
+  addDialog.open = false;
+  editDialog.open = false;
+});
+
+closeEditDialog.addEventListener("click", () => {
+  console.log("i'm listen");
+  addDialog.open = false;
+  editDialog.open = false;
+});
+
+let indexDialog;
+
+function handleOpenDelete(index) {
+  deleteDialog.open = true;
+  indexDialog = index;
+  deleteMsg.innerText = `Do You Want to Delete "${todos[index].title}" From To Do List are you Sure?`;
+}
+function handelEditTodo(index) {
+  editDialog.open = true;
+  indexDialog = index;
+  inputEdit.value = todos[index].title;
+}
+
+deleteBtn.addEventListener("click", () => {
+  todos.splice(indexDialog, 1);
+  deleteDialog.open = false;
+  storeTodos();
+  getTodos();
+});
+
+editBtn.addEventListener("click", () => {
+  todos[indexDialog].title = inputEdit.value;
+  editDialog.open = false;
+  storeTodos();
+  getTodos();
+});
+cancelBtn.addEventListener("click", () => {
+  deleteDialog.open = false;
+});
+
+function handleDeleteItem(index) {
+  deleteDialog.open = false;
+}
+
+const handleCheckTodo = (index) => {
+  console.log(index);
+  todos[index].check = !todos[index].check;
+  storeTodos();
+  getTodos();
+  console.log(todos);
+};
+
+addDialogBtn.addEventListener("click", () => {
+  let id = todos.length;
+
+  let newTask = {
+    id,
+    title: inputAdd.value,
+    check: false,
+  };
+  todos.push(newTask);
+  storeTodos();
+  getTodos();
+  inputAdd.value = "";
+  addDialog.open = false;
+});
